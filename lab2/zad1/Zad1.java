be95004fb430b7cc7d5d85524d6aedeb8b18046b
Michał Nożkiewicz <michno513@gmail.com>
2023-11-02 04:59:10 +0100

    lab2_done

--- lab2/zad1/Zad1.java
+++ lab2/zad1/Zad1.java
@@ -1,4 +1,4 @@
-package lab2;
+package lab2.zad1;
 
 
 import java.util.*;
--- lab2/zad2/Zad2.java
+++ lab2/zad2/Zad2.java
@@ -1,4 +1,4 @@
-package lab2;
+package lab2.zad2;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
